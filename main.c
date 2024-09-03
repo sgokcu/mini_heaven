61b46c434f423825a77f53b474585442b91aff4a
Simal Gokcu <sgokcu@k1m30s01.42istanbul.com.tr>
2024-09-03 16:32:59 +0300

    '$'pwd

--- main.c
+++ main.c
@@ -25,6 +25,8 @@ int	main()
 		add_history(temp);
 		temp2 = ft_strtrim(temp, " \t");
 		free(temp);
+		if (!temp2[0])
+			continue;
 		if (!is_quotes_closed(temp2))
 			continue ;
 		ft_signal_regulator(MAIN_P2);
@@ -33,6 +35,11 @@ int	main()
 		if (!is_valid_name(temp2, &mini))
 			continue ;
 		temp3 = is_dollar_exist_and_valid(temp2, &mini);
+		if (!temp3 || !temp3[0])
+		{
+			free (temp3);
+			continue;
+		}
 		args = mm_split(temp3, '|');
 		placing(args, &mini);
 		read_and_exec(&mini, command_list_count(&mini));
